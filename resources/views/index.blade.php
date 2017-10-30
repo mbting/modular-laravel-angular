33bb253edbc9133459dabbf497c0e78c39f516f9
Ibrahim Naeem <mbting.9m@gmail.com>
2017-10-30 12:28:44 +0500

    integrate angular js

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,11 @@
     <title>Phoenix</title>
 </head>
 <body>
-    it works
+  <app-root>loading</app-root>
+  <script type="text/javascript" src="js/inline.bundle.js"></script>
+  <script type="text/javascript" src="js/polyfills.bundle.js"></script>
+  <script type="text/javascript" src="js/styles.bundle.js"></script>
+  <script type="text/javascript" src="js/vendor.bundle.js"></script>
+  <script type="text/javascript" src="js/main.bundle.js"></script></body>
 </body>
 </html>
