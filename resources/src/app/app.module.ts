c986085011a36cc354e5cb9db6b9d2280a0dd0de
Ibrahim Naeem <mbting.9m@gmail.com>
2017-11-19 17:50:21 +0500

    added login page and responsive side nav

--- resources/src/app/app.module.ts
+++ resources/src/app/app.module.ts
@@ -9,13 +9,15 @@ import {DashboardComponent} from './dashboard/dashboard.component';
 import {SampleModule} from '../../../Modules/Sample/Resources/src/sample.module';
 import {HeaderComponent} from './header/header.component';
 import {SidebarComponent} from './sidebar/sidebar.component';
+import {LoginComponent} from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
     HeaderComponent,
     SidebarComponent,
+    AppComponent,
+    LoginComponent,
     DashboardComponent
   ],
   imports: [
