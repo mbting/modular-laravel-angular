af60252a0967e071b5c1179e8721a0ccf6e109d9
Ibrahim Naeem <mbting.9m@gmail.com>
2018-01-20 14:46:31 +0500

    return navigation from module.json

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Http\Responses\Response;
+use Module;
 
 class HomeController extends Controller {
 
   public function index() {
     return Response::success();
+  }
+
+  public function sidebar() {
+    $nav = [];
+    foreach (Module::collections() as $module) {
+      $config = json_decode(file_get_contents($module->getPath() . DIRECTORY_SEPARATOR . 'module.json'));
+      $nav[] = ['name' => $module->getName(), 'children' => $config->action];
+    }
+    return Response::success($nav);
   }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ Route::group(['prefix' => 'api'], function () {
 
   Route::middleware(['auth.check'])->group(function () {
     Route::get('/',['uses' => 'HomeController@index', 'as' => 'api.index']);
+    Route::get('/sidebar',['uses' => 'HomeController@sidebar', 'as' => 'api.sidebar']);
     Route::get('/logout',['uses' => 'AuthenticationController@doLogout', 'as' => 'api.logout']);
   });
   Route::post('/login',['uses' => 'AuthenticationController@doLogin', 'as' => 'api.login']);
